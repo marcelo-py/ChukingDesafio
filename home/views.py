d3d5a2262d997a697a401ae2c62e56bad7eb097e
marcelo-py <marcelo.araujo.999s@gmail.com>
2023-08-29 01:49:41 -0300

    finalizadissimo

--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import tempfile
 import os
-from django.conf import settings
 
 
 def index(request):
@@ -10,21 +8,23 @@ def index(request):
         chunk = request.FILES.get('chunk')
         total_chunks = int(request.POST.get('total_chunks'))
         chunk_number = int(request.POST.get('chunk_number'))
+        file_name = request.POST.get('file-name')
 
-        # Salvar o chunk em um local temporário no diretório da view
+        # local temporário no diretório da view
         view_dir = os.path.dirname(os.path.abspath(__file__))
-        temp_file_path = os.path.join(view_dir, f'chunk_{chunk_number}.tmp')
-
+        temp_file_path = os.path.join(view_dir+'/chunk_temp/', f'chunk_{chunk_number}.tmp')
+        
+        print('Chunnnk>>>>>>>>', file_name)
         with open(temp_file_path, 'wb') as temp_file:
             temp_file.write(chunk.read())
 
         if chunk_number == total_chunks - 1:
-            # Todos os chunks foram enviados
-            final_file_path = os.path.join(view_dir, 'final_file.mp4')
+            final_file_path = os.path.join(view_dir+'/final_files/', file_name)
             with open(final_file_path, 'wb') as final_file:
                 for i in range(total_chunks):
-                    chunk_path = os.path.join(view_dir, f'chunk_{i}.tmp')
-
+                    chunk_path = os.path.join(view_dir+'/chunk_temp/', f'chunk_{i}.tmp')
+                    
+                    # Escreve a proxima parte do arquivo no mesmo anterior 
                     with open(chunk_path, 'rb') as chunk_file:
                         final_file.write(chunk_file.read())
 
